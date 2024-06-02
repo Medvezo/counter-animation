977f93eaabe670db5c72d6ddc601f1e49497a78b
Mansur Mustafayev <mansur.mustafayev@gmail.com>
2024-06-02 19:21:19 +0400

    feat: add SlowEnd type of Counter by Prop

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,33 @@ import Counter from "./components/Counter";
 
 const HomePage: React.FC = () => {
 	return (
-		<main className="w-full min-h-screen flex justify-center items-center flex-wrap flex-col lg:flex-row ">
+		<main className="w-full min-h-screen flex justify-center items-center gap-10 lg:gap-16 flex-wrap flex-col lg:flex-row ">
 			<div className="flex flex-col justify-start items-center gap-5 ">
 				<h2 className="text-4xl">Simple Counter</h2>
-				<span className="text-3xl font-bold " id="counter1">0</span>
+				<span className="text-3xl font-bold " id="simple-counter">
+					0
+				</span>
 				<Counter
-					selector="counter1"
+					selector="simple-counter"
 					current={0}
 					target={100}
 					duration={3000}
 					letter="+"
+				/>
+			</div>
+
+			<div className="flex flex-col justify-start items-center gap-5 ">
+				<h2 className="text-4xl">Slow-end Counter</h2>
+				<span className="text-3xl font-bold " id="slow-end-counter">
+					0
+				</span>
+				<Counter
+					selector="slow-end-counter"
+					current={0}
+					target={300}
+					duration={3000}
+					letter="$"
+					slowEnd
 				/>
 			</div>
 		</main>
--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,4 +1,3 @@
-// Counter.tsx
 import React, { useEffect, useState } from "react";
 
 interface CounterProps {
@@ -7,6 +6,7 @@ interface CounterProps {
 	target: number;
 	duration: number;
 	letter?: string;
+	slowEnd?: boolean;
 }
 
 const Counter: React.FC<CounterProps> = ({
@@ -15,21 +15,35 @@ const Counter: React.FC<CounterProps> = ({
 	target,
 	duration,
 	letter = "",
+	slowEnd = false,
 }) => {
 	const [count, setCount] = useState(current);
 
 	useEffect(() => {
+		const stepTime = Math.abs(Math.floor(duration / target));
+
 		const increment = () => {
 			if (count < target) {
 				setCount((prevCount) => prevCount + 1);
 			}
 		};
 
-		const stepTime = Math.abs(Math.floor(duration / target));
-		const timer = setInterval(increment, stepTime);
+		const adjustStepTime = () => {
+			if (slowEnd && count > target * 0.9) {
+				const remaining = target - count;
+				const slowdownFactor = Math.pow(
+					1.5,
+					10 * (1 - remaining / (target * 0.1))
+				); 
+				return stepTime * slowdownFactor;
+			}
+			return stepTime;
+		};
 
-		return () => clearInterval(timer);
-	}, [count, target, duration]);
+		const timer = setTimeout(increment, adjustStepTime());
+
+		return () => clearTimeout(timer);
+	}, [count, target, duration, slowEnd]);
 
 	useEffect(() => {
 		const counterElement = document.getElementById(selector);
